ee8162f8726daebd62bdb98a419b7ff104d45af0
MaulinSurati <136084577+MaulinSurati@users.noreply.github.com>
2024-10-14 15:28:03 +0530

    Add files via upload

---
+++ face_recognition_attendance_system.py
@@ -0,0 +1,157 @@
+import face_recognition
+import cv2
+import numpy as np
+import os
+import xlrd
+from xlutils.copy import copy as xl_copy
+from datetime import date
+import xlwt
+from datetime import datetime
+
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+
+# Load a sample picture and learn how to recognize it.
+image = 'C:/Dell_backup/D/NIRMA/SEM_3/DMV/INNOVATIVE/final/Smart_Attendence_System/bhavya.jpeg'
+image2 = 'C:/Dell_backup/D/NIRMA/SEM_3/DMV/INNOVATIVE/final/Smart_Attendence_System/sahil.jpeg'
+image3 = 'C:/Dell_backup/D/NIRMA/SEM_3/DMV/INNOVATIVE/final/Smart_Attendence_System/maulin.jpg'
+image4 = 'C:/Dell_backup/D/NIRMA/SEM_3/DMV/INNOVATIVE/final/Smart_Attendence_System/aastha.jpeg'
+image5 = 'C:/Dell_backup/D/NIRMA/SEM_3/DMV/INNOVATIVE/final/Smart_Attendence_System/tanay.jpeg'
+
+person1_name = "018_bhavya"
+person1_image = face_recognition.load_image_file(image)
+person1_face_encoding = face_recognition.face_encodings(person1_image)[0]
+
+person2_name = "056_Sahil"
+person2_image = face_recognition.load_image_file(image2)
+person2_face_encoding = face_recognition.face_encodings(person2_image)[0]
+
+person3_name = "064_Maulin"
+person3_image = face_recognition.load_image_file(image3)
+person3_face_encoding = face_recognition.face_encodings(person3_image)[0]
+
+person4_name = "054_Aastha"
+person4_image = face_recognition.load_image_file(image4)
+person4_face_encoding = face_recognition.face_encodings(person4_image)[0]
+
+person5_name = "070_Tanay"
+person5_image = face_recognition.load_image_file(image4)
+person5_face_encoding = face_recognition.face_encodings(person4_image)[0]
+
+known_face_encodings = [
+    person1_face_encoding,
+    person2_face_encoding,
+    person3_face_encoding,
+    person4_face_encoding,
+    person5_face_encoding
+]
+known_face_names = [
+    person1_name,
+    person2_name,
+    person3_name,
+    person4_name,
+    person5_name
+]
+
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+rb = xlrd.open_workbook('C:/Dell_backup/D/NIRMA/SEM_3/DMV/INNOVATIVE/final/Smart_Attendence_System/attendence_excel.xls', formatting_info=True)
+wb = xl_copy(rb)
+inp = input('Please give the current subject lecture name: ')
+sheet1 = wb.add_sheet(inp)
+sheet1.write(0, 0, "Name")
+sheet1.write(0, 1, "Attendance")
+sheet1.write(0, 2, "Date")
+sheet1.write(0, 3, "Time")
+row = 1
+col = 0
+already_attendance_taken_set = set()
+
+try:
+    while True:
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
+
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
+
+        # Only process every other frame of video to save time
+        if process_this_frame:
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
+
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+
+                face_names.append(name)
+
+                if name != "Unknown" and name not in already_attendance_taken_set:
+                    current_time = datetime.now().strftime("%H:%M:%S")
+                    current_date = datetime.now().strftime("%d-%m-%Y")
+                    sheet1.write(row, col, name)
+                    col = col + 1
+                    sheet1.write(row, col, "Present")
+                    col = col + 1
+                    sheet1.write(row, col, current_date)  # Add current time to the sheet
+                    col = col + 1
+                    sheet1.write(row, col, current_time)
+                    row += 1
+                    col = 0
+                    print("Attendance taken for", name)
+                    wb.save('C:/Dell_backup/D/NIRMA/SEM_3/DMV/INNOVATIVE/final/Smart_Attendence_System/attendence_excel.xls')
+                    already_attendance_taken_set.add(name)
+                else:
+                    if name != "Unknown":
+                        print("Attendance already taken for", name)
+                    else:
+                        print("Face not recognized")
+
+        process_this_frame = not process_this_frame
+
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            print("Data saved")
+            break
+
+except KeyboardInterrupt:
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
